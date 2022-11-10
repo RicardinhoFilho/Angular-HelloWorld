38876a2e4b8670cf966ecb292b4dcdc4b8498549
Ricardo <ricardinhogiasson16@gmail.com>
2022-11-10 15:26:28 -0300

    Adicionando Modulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import * as mod1 from "./modulo1";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = mod1.nome;
+  pessoa = new mod1.Pessoa();
 }
---
+++ src/app/modulo1/modulo1.ts
@@ -0,0 +1,5 @@
+export var nome:string = "Ricardo Pinto Giasson Filho";
+
+export class Pessoa{
+    nome:string = "Teste";
+}
